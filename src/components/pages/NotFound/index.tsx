0b868a751dcefbcd9c320edcf5ad80cb0162e026
RLT-G <dop.inco.k.b@gmail.com>
2024-11-29 18:30:46 +0300

    pretty 404

--- src/components/pages/NotFound/index.tsx
+++ src/components/pages/NotFound/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "../../Header";
 import classes from './index.module.css'
+import Button from "../../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
+    const navigate = useNavigate()
     return (
         <>
             <Header isMainPage={false}/>
             
-            <div className={classes.Text}>Not Found 404</div>
+            <div className={classes.E404Container}>
+                <span className={classes.E404}>404</span>
+                <span className={classes.E404Text}>Страница не найдена</span>
+                <Button className={classes.Button} 
+                    onClick={() => {navigate('/')}}>Вернуться на главную</Button>
+            </div>
         </>
     )
 }
